a25137dc0ef0aa09d98e36f7b619c047cf4dac0a
Archenoth <archenoth@gmail.com>
2017-12-11 18:42:14 -0700

    The Art Relay now persists artists between bot restarts
    When the art relay is used, a new file called "art_relay.yaml" will be
    created and written to. This file will contain the aritsts that can be
    picked from.

--- subsystems/art_relay.rb
+++ subsystems/art_relay.rb
@@ -1,17 +1,43 @@
 #!/usr/bin/ruby
 require 'set'
+require 'yaml'
 
 # The Module for the art relay where one artist draws a thing, and
 # when they are done, another random artist is picked
 module ArtRelay
+  # Loads arists from a YAML file. Default is art_relay.yaml, though
+  # you can use whatever filename you like as the first argument
+  def load_artists(file="art_relay.yaml")
+    @artists = YAML.load_file(file)
+  end
+
+  # Save the current artists to a file as YAML. Default is
+  # art_relay.yaml, though you can override this with the first and
+  # only argument
+  def save_artists(file="art_relay.yaml")
+    IO.write(file, @artists.to_yaml)
+  end
+
+  # Gets the currently registered artists. Loads from the default file
+  # if there is no member in this class, and just creates a new Set
+  # object if that isn't a thing either
   def artists
-    @artists = @artists || Set.new
+    if @artists.nil? then
+      begin
+        @artists = load_artists
+      rescue Errno::ENOENT
+        @artists = Set.new
+      end
+    else
+      @artists
+    end
   end
 
   # Registers an artist to be picked from during the rotation
   def bot_art_me(event)
     if artists.add?(event.message.author.id) then
       event.respond "arrite! added now! i will call u wen its drawing time!!"
+      save_artists
     else
       event.respond "you want me to add you again?? okay! okay! i added you again..."
       event.respond "...but not really"
@@ -23,6 +49,7 @@ module ArtRelay
     id = event.message.author.id
     if artists.delete?(id) then
       event.respond "okay! buh bye!"
+      save_artists
 
       if @current_artist == name then
         event.respond "...wait, u r the current artist! that means the new one should beeee...."
